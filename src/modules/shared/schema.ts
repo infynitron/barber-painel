c0852d7643c05d4335294116a34078210e322616
David Marques <davidmarques.261299@gmail.com>
2025-11-27 17:11:27 -0300

    fix: Correção da mensagem de erro do schema

--- src/modules/shared/schema.ts
+++ src/modules/shared/schema.ts
@@ -2,20 +2,20 @@ import { z } from "zod/v4";
 
 export const password = z
   .string()
-  .refine((value) => /[a-z]/.test(value), {
-    message: "A senha deve conter pelo menos uma letra minúscula",
-    path: ["password"],
-  })
-  .refine((value) => /[A-Z]/.test(value), {
-    message: "A senha deve conter pelo menos uma letra maiúscula",
-    path: ["password"],
-  })
-  .refine((value) => /[0-9]/.test(value), {
-    message: "A senha deve conter pelo menos um número",
-    path: ["password"],
-  })
-  .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
-    message: "A senha deve conter pelo menos um caractere especial",
-    path: ["password"],
-  })
-  .min(6, { message: "A senha deve ter no mínimo 6 caracteres" });
+  .refine(
+    (value) => /[a-z]/.test(value),
+    "A senha deve conter pelo menos uma letra minúscula"
+  )
+  .refine(
+    (value) => /[A-Z]/.test(value),
+    "A senha deve conter pelo menos uma letra maiúscula"
+  )
+  .refine(
+    (value) => /[0-9]/.test(value),
+    "A senha deve conter pelo menos um número"
+  )
+  .refine(
+    (value) => /[!@#$%^&*(),.?":{}|<>]/.test(value),
+    "A senha deve conter pelo menos um caractere especial"
+  )
+  .min(6, "A senha deve ter no mínimo 6 caracteres");
