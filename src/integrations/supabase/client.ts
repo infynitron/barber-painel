64de2c017de7d2a709d072bfac575c39dd240b00
David Marques <davidmarques.261299@gmail.com>
2025-11-30 23:49:19 -0300

    feat: Ação de cadastro

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,6 +3,26 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true,
+      },
+      global: {
+        headers: { 
+          'x-application-name': 'infinytron-agenda',
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization, apikey, x-client-info'
+        }
+      },
+    }
   );
 }
+
+export const supabase = createClient();
--- src/modules/auth/ui/SignUpGoogle.tsx
+++ src/modules/auth/ui/SignUpGoogle.tsx
@@ -1,17 +1,45 @@
 "use client";
 
 import React from "react";
+import { toast } from "sonner";
 import { LoaderIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 import { GoogleIcon } from "@/icons";
 
+import { supabase } from "@/integrations/supabase/client";
+
 export default function SignUpGoogle() {
   const [isGoogleLoading, setIsGoogleLoading] = React.useState<boolean>(false);
 
+  const handleSignUpWithGoogle = async () => {
+    setIsGoogleLoading(true);
+
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: window.location.origin,
+      },
+    });
+    if (error) {
+      console.error(error);
+      toast.error(
+        error.message ?? "Ocorreu um erro ao tentar fazer login com o Google"
+      );
+    }
+
+    setIsGoogleLoading(false);
+  };
+
   return (
-    <Button type="button" variant="outline" className="flex mx-auto">
+    <Button
+      className="flex mx-auto"
+      type="button"
+      variant="outline"
+      disabled={isGoogleLoading}
+      onClick={handleSignUpWithGoogle}
+    >
       {isGoogleLoading ? (
         <LoaderIcon className="animate-spin size-5" />
       ) : (
