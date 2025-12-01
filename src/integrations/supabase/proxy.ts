33207a7bc4edc64677b8b690845526aae114892e
David Marques <davidmarques.261299@gmail.com>
2025-12-01 03:20:03 -0300

    refactor: Renomeado middleware para proxy

--- src/integrations/supabase/proxy.ts
+++ src/integrations/supabase/proxy.ts
@@ -21,8 +21,8 @@ export async function updateSession(request: NextRequest) {
           supabaseResponse = NextResponse.next({
             request,
           });
-          cookiesToSet.forEach(({ name, value }) =>
-            supabaseResponse.cookies.set(name, value)
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
           );
         },
       },
@@ -30,10 +30,10 @@ export async function updateSession(request: NextRequest) {
   );
 
   const { data } = await supabase.auth.getClaims();
-
   const user = data?.claims;
 
-  if (!user && !request.nextUrl.pathname.startsWith("/sign-in")) {
+  const pathname = request.nextUrl.pathname;
+  if (pathname !== "/" && !user && !pathname.startsWith("/sign-in")) {
     const url = request.nextUrl.clone();
     url.pathname = "/sign-in";
     return NextResponse.redirect(url);
