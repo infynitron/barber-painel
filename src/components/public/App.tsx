d8b287a8b1e426fb83f55adb6feb0c26e46a1fe6
CrisDevv <cris.vr.pontagrossa@gmail.com>
2025-12-01 23:25:24 -0300

    Refacter de componente nao usado

--- src/components/public/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <section className="py-20 md:py-32 bg-secondary/30">
-      <div className="container mx-auto px-4">App</div>
-    </section>
-  );
-}
